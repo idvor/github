5e432ff30d9a7551f6164a506fe509b32192505d
Darko <Darko@DESKTOP-9602NLK>
2023-11-28 00:42:40 +0100

    Add project files.

---
+++ github/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+
+int speedlimit = 50;
+if (speedlimit < 80)
+{
+    Console.WriteLine("you are good driver");
+}
+else
+{
+    Console.WriteLine("you are bad driver");
+}
